9c55b13122ad452882d3170f311a51c690504aa2
Farhan <farhan.khan@thisisgravitas.com>
2023-04-26 16:03:46 +0500

    express js has added, script set and ejs installed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hello World');
+import express from 'express';
+import path from 'path';
+
+const app = express();
+
+/* Middleware */
+
+/* Setting up the view engine */
+app.set("view engine", "ejs")
+
+app.listen(5000, () => {
+    console.log('Server is working');
+});
+
+app.get('/', (req, res) => {
+    res.render('index', { value: "some value from BE" });
+});
